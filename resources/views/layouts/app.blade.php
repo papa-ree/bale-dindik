9f270e982cc2918b6dc859b5422c32924a09b9f9
Papa Ree <79291092+papa-ree@users.noreply.github.com>
2025-12-30 13:14:33 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,13 +22,13 @@
     @livewireStyles
 
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-P0SERKMDGG"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-H9JWC4EMHQ"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag () { dataLayer.push( arguments ); }
         gtag( 'js', new Date() );
 
-        gtag( 'config', 'G-P0SERKMDGG' );
+        gtag( 'config', 'G-H9JWC4EMHQ' );
     </script>
 
 </head>
